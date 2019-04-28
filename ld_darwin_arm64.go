bd809989953032c225b64d87233ff87c755db8a5
a97077088 <97077088@qq.com>
2019-04-29 04:06:16 +0800

    aa

---
+++ ld_darwin_arm64.go
@@ -0,0 +1,6 @@
+package gogadget
+
+/*
+#cgo LDFLAGS: -fPIC -Wall -Os -pipe -g3 -L./lib -lios64 -lm -ldl -ldl -lresolv -lm -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/iPhoneOS11.4.sdk -Wl,-iphoneos_version_min,7.0 -arch arm64 -Wl,-dead_strip
+*/
+import "C"
