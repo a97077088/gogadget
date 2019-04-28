197911499579e0563361df0a2e1cfbc03ca6e725
a97077088 <97077088@qq.com>
2019-04-29 03:49:53 +0800

    Update and rename ld_darwinx64.go to ld_darwin_amd64.go

---
+++ ld_darwin_amd64.go
@@ -0,0 +1,6 @@
+package gogadget
+
+/*
+#cgo LDFLAGS: -fPIC -Wall -Os -pipe -g3 -L./lib -lfrida-gumjs -lm -ldl -ldl -lresolv -lm -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk -Wl,-macosx_version_min,10.9 -arch x86_64 -Wl,-dead_strip
+*/
+import "C"
