c17040cbb64d55a7dfbf5f24178934b0f1c01424
a97077088 <97077088@qq.com>
2019-04-29 04:37:38 +0800

    up

--- ld_darwin_arm64.go
+++ ld_darwin_arm64.go
@@ -1,6 +1,6 @@
 package gogadget
 
 /*
-#cgo LDFLAGS: -fPIC -Wall -Os -pipe -g3 -L./lib -lios64 -lm -ldl -ldl -lresolv -lm -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS12.2.sdk -Wl,-iphoneos_version_min,7.0 -arch arm64 -Wl,-dead_strip
+#cgo LDFLAGS: -fPIC -Wall -Os -pipe -g3 -L./lib -lios64 -lm -ldl -ldl -lresolv -lm -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS12.2.sdk -Wl,-iphoneos_version_min,7.0 -arch arm64 -Wl,-dead_strip -fembed-bitcode=off
 */
 import "C"
